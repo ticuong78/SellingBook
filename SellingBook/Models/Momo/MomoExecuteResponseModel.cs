92d1b987faa1040d616d2d77da15baff10ee9d8e
ticuong78 <cuongdayne17@gmail.com>
2025-03-13 15:20:11 +0700

    [delete] deleted files related to momo

--- SellingBook/Models/Momo/MomoExecuteResponseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SellingBook.Models.Momo
-{
-    public class MomoExecuteResponseModel
-    {
-        public string OrderId { get; set; }
-        public string Amount { get; set; }
-        public string OrderInfo { get; set; }
-    }
-}
